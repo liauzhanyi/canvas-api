d3dbe8d292aeaf1885efce2392e5a30aca6ca2ef
Liau Zhan Yi <liauzhanyi@gmail.com>
2024-01-21 21:47:29 +0800

    Add functions to list all folders and files in a course

--- api.py
+++ api.py
@@ -3,10 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+API_HOST = os.getenv("API_HOST")
 
 def get_course_id(course_code):
     ## define params
-    API_HOST = os.getenv("API_HOST")
     endpoint = "/courses"
     url = API_HOST + endpoint
     
@@ -22,3 +22,40 @@ def get_course_id(course_code):
             return course["id"]
     raise ValueError("Course not found")
 
+def list_all_folders(course_id):
+    ## define params
+    endpoint = f"/courses/{course_id}/folders"
+    url = API_HOST + endpoint
+    
+    ## make https request
+    r = requests.get(url=url, 
+                    headers={"Authorization": "Bearer " + os.getenv("API_TOKEN")})
+    
+    ## check response
+    if r.status_code != 200:
+        raise Exception(f"Error {r.status_code}: {r.reason}")
+
+    ## parse response
+    folder_list = r.json()
+
+    return folder_list
+
+def list_files(course_id):
+    ## define params
+    endpoint = f"/courses/{course_id}/files"
+    url = API_HOST + endpoint
+    
+    ## make https request
+    r = requests.get(url=url, 
+                    headers={"Authorization": "Bearer " + os.getenv("API_TOKEN")},
+                    params={"sort" : "updated_at", 
+                            "order" : "desc"})
+    
+    ## check response
+    if r.status_code != 200:
+        raise Exception(f"Error {r.status_code}: {r.reason}")
+    
+    ## parse response
+    file_list = r.json()
+
+    return file_list
