f9c1380bbc15101321e2a0e9e0a9102a9a799711
Liau Zhan Yi <liauzhanyi@gmail.com>
2024-01-28 13:16:14 +0800

    Add file download functionality

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 import requests
 import os
+from urllib.request import urlretrieve
 from dotenv import load_dotenv
 from utils import refresh_cache
+from datetime import datetime
 
 load_dotenv()
 API_HOST = os.getenv("API_HOST")
@@ -32,6 +34,7 @@ def get_course_id(course_code):
             ## update cache
             id = course["id"]
             cache.update(key, id)
+            cache.store()
             return id
 
     ## if id not in cache or api == course not found, raise error
@@ -75,4 +78,27 @@ def list_files(course_id):
 
     return file_list
 
-cache.store()
+def download_course_files(courses):
+    last_update = cache.read("last_update")
+    
+    ## if last_update not in cache, set to long time ago 1970-01-01 should suffice
+    if not last_update:
+        last_update = datetime(1970, 1, 1)
+
+    for course in courses:
+        id = get_course_id(course)
+        files = list_files(id)
+        
+        ## download file if file is updated after last updated datetime
+        for file in files:
+            if datetime.fromisoformat(file["updated_at"][:-1]) >= last_update:
+                url = file["url"]
+                fname = file["display_name"]
+                urlretrieve(
+                    url, 
+                    os.path.join(os.getenv("ROOT"), os.getenv("NEW_FILES_FOLDER"), fname)
+                    )
+    
+    cache.update("last_update", datetime.now())
+    cache.store()
+    return 200
