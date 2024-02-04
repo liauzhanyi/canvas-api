b0a6fab84160259641a0099c6eb6f90236536a7a
Liau Zhan Yi <liauzhanyi@gmail.com>
2024-02-04 20:37:44 +0800

    Add logging functionality to main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import argparse
 from api import download_course_files
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
+
+LOG_FILE = os.path.join(os.getenv("ROOT"), "logs.txt")
+f = open(LOG_FILE, "w")
 
 ## define command line arguments
 argparser = argparse.ArgumentParser()
@@ -10,10 +18,14 @@ argparser.add_argument("courses", type=str, nargs="+",
 parser = argparser.parse_args()
 
 ## start
-print("programme started. downloading new files...")
+print("programme started. downloading new files...", file=f)
 
+## FIXME: last_updated will cause new course files to not be fully downloaded
 ## download files
 download_course_files(parser.courses)
 
 ## end
-print("programme ended successfully")
+print("programme ended successfully", file=f)
+print(f"time {datetime.now()}", file=f)
+
+f.close()
